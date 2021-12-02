bccf28129b42111bb7d3e35d778d0a58e4bdd0b3
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-02 17:31:18 +0100

    Aggiunte modifiche file index.php (Snack 1)

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,6 +1,6 @@
 <?
 /* 
-Snack 1:
+Traccia Snack 1:
 Creiamo un array contenente le partite di basket di un’ipotetica tappa 
 del calendario. Ogni array avrà una squadra di casa e una squadra ospite,
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
@@ -8,26 +8,28 @@ Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 */
 
-$first_match = [
-     "casa" => "Olimpia Milano",
-     "ospite" => "Cantù",
-     "punti squadra di casa" => "55",
-     "punti squadra di ospite" => "60",
-    ];
+$matches = [ 
+    [
+     "local_team" => "Olimpia Milano",
+     "visitor_team" => "Cantù",
+     "local_points" => "55",
+     "visitor_points" => "60",
+    ],
 
-    $second_match = [
-     "casa" => "Virtus Bologna",
-     "ospite" => "Varese",
-     "punti squadra di casa" => "45",
-     "punti squadra di ospite" => "94",
-    ];
+    [
+     "local_team" => "Virtus Bologna",
+     "visitor_team" => "Varese",
+     "local_points" => "45",
+     "visitor_points" => "94",
+    ],
 
-    $third_match = [
-     "casa" => "Virtus Roma",
-     "ospite" => "Fortitudo Bologna",
-     "punti squadra di casa" => "84",
-     "punti squadra di ospite" => "65",
-    ];
+    [
+     "local_team" => "Virtus Roma",
+     "visitor_team" => "Fortitudo Bologna",
+     "local_points" => "84",
+     "visitor_points" => "65",
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -36,19 +38,16 @@ $first_match = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 1</title>
 </head>
 <body>
     <h1>Campionato di basket</h1>
-
-    <h2>Primo match: </h2>
-    <p><?= $first_match[0].' - '.$first_match[1].' | '.$first_match[2].' - '.$first_match[3] ?></p>
-
-    <h2>Secondo match: </h2>
-    <p><?= $second_match[0].' - '.$second_match[1].' | '.$second_match[2].' - '.$first_match[3] ?></p>
-
-    <h2>Terzo match: </h2>
-    <p><?= $third_match[0].' - '.$third_match[1].' | '.$third_match[2].' - '.$third_match[3] ?></p>
+    <?php for ($i=0; $i < count($matches); $i++) {?>
+        <h2><?php echo $matches[i]["local_team"]; ?></h2>
+        <h2><?php echo $matches[i]["visitor_team"]; ?></h2>
+        <h2><?php echo $matches[i]["local_points"]; ?></h2>
+        <h2><?php echo $matches[i]["visitor_points"]; ?></h2>
+    <? } ?>
 </body>
 </html>
 
