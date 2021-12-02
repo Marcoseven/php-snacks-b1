1acf333fd578a784dc528cd088eb3db7dba9443e
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-02 17:32:08 +0100

    Aggiunte modifiche file index.php (Snack 2)

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,20 +1,20 @@
 <?php
 /* 
-Traccia:
+Traccia Snack 2:
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che 
 name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
 Strumenti:
-- GET,
-- strlen(), 
-- strpo(),
-- ...
+- GET;
+- strlen(); 
+- strpo();
+- is_numeric().
 */
 
-$name = $_GET[$name];
-$mail = $_GET[$mail];
-$age = $_GET[$age];
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
 ?>
 
 <!DOCTYPE html>
@@ -23,26 +23,15 @@ $age = $_GET[$age];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 2</title>
 </head>
 <body>
+    <h1>Condizioni di accesso</h1>
 
-<h1>Condizioni di accesso</h1>
-<h2>Accesso per il nome: </h2>
-    <?php 
-    if (strlen($name) > 3){ ?>
-        <h3>Accesso Riuscito</h3>
+    <?php if(strlen($name) > 3 && (strpos($mail, '@') && strpos($mail, '.')) && is_numeric($age)){?>
+        <h2>Accesso riuscito</h2>
     <?php } else{ ?>
-        <h3>Accesso Negato</h3>
-    <?php } ?>
-
-<h2>Accesso per l'email: </h2>
-    <?php 
-    if (strpos($mail, '@') && strpos(email, '.')){ ?>
-        <h3>Accesso Riuscito</h3>
-    <?php } else{ ?>
-        <h3>Accesso Negato</h3>
+        <h2>Accesso negato</h2>
     <?php } ?>
 </body>
 </html>
-
